e7b3262721ca4deca7ea1b6466137d85cccc5900
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-26 20:19:10 +0600

    aidl: vibrator: implement indexed and duration based haptics
    Change-Id: I41453edb093d6aaf1c74179c5a1ec016becabd65
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- aidl/vibrator/include/vibrator-impl/Vibrator.h
+++ aidl/vibrator/include/vibrator-impl/Vibrator.h
@@ -1,27 +1,26 @@
 /*
  * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2023 StatiXOS
+ * SPDX-License-Identifer: Apache-2.0
  */
 
 #pragma once
 
 #include <aidl/android/hardware/vibrator/BnVibrator.h>
+#include <aidl/android/hardware/vibrator/BnVibratorManager.h>
 
 namespace aidl {
 namespace android {
 namespace hardware {
 namespace vibrator {
+
+class VibratorManager : public BnVibratorManager {
+    VibratorManager(std::shared_ptr<IVibrator> vibrator) : mDefaultVibrator(std::move(vibrator)){};
+    ndk::ScopedAStatus getVibrator(int32_t vibratorId,
+                                std::shared_ptr<IVibrator>* _aidl_return) override;
+    private:
+        std::shared_ptr<IVibrator> mDefaultVibrator;
+};
 
 class Vibrator : public BnVibrator {
     ndk::ScopedAStatus getCapabilities(int32_t* _aidl_return) override;
